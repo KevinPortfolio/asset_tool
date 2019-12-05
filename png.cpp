dddf1f971b35efefa34b993b7c69fa84cc9bb92d
Kevin Baker <KevinLB88Work@gmail.com>
2019-12-04 20:00:28 -0800

    Fixing naming scheme

--- png.cpp
+++ png.cpp
@@ -8,268 +8,275 @@
 
 struct DeflateBlock
 {
-  uint32 Length;
-  uint32 BytesPerPixel;
-  uint32 EndIndexPos;
-  uint32 ScanLinePos;
-  uint32 ScanLineWidth;
-  uint8 CurrentFilter;
+  uint32 length;
+  uint32 bytes_per_pixel;
+  uint32 end_index_pos;
+  uint32 scan_line_pos;
+  uint32 scan_line_width;
+  uint8 current_filter;
 };
 
-static int32
-PaethPredictor(int32 Previous, int32 Above, int32 AbovePrev)
+static inline int32
+png_math_abs_val(int32 value)
 {
-  int32 p = Previous + Above - AbovePrev;
-  int32 pa = Math_AbsVal(p - Previous);
-  int32 pb = Math_AbsVal(p - Above);
-  int32 pc = Math_AbsVal(p - AbovePrev);
+  return((value < 0) ? -value: value);
+}
+
+static int32
+paeth_predictor(int32 previous, int32 above, int32 above_prev)
+{
+  int32 p = previous + above - above_prev;
+  int32 pa = png_math_abs_val(p - Previous);
+  int32 pb = png_math_abs_val(p - Above);
+  int32 pc = png_math_abs_val(p - AbovePrev);
+
   if ((pa <= pb) && (pa <= pc))
   {
-    return Previous;
+    return previous;
   }
   else if (pb <= pc)
   {
-    return Above;
+    return above;
   }
   else
   {
-    return AbovePrev;
+    return above_prev;
   }
 }
 
-static void
-NoFilter(uint32* Index, uint8* Data, uint32* ImgIndex,
-	 uint8* ImageData, DeflateBlock* BlockState)
+static inline void
+no_filter(uint32* index, uint8* data, uint32* image_index, uint8* image_data,
+	  DeflateBlock* block_state)
 {
-  ImageData[*ImgIndex] = Data[*Index];
-  *ImgIndex = *ImgIndex + 1;
-}
-
-static void 
-SubFilter(uint32* Index, uint8* Data, uint32* ImgIndex,
-	  uint8* ImageData, DeflateBlock* BlockState) 
-{
-  if (BlockState->ScanLinePos > 2)
-  {
-    ImageData[*ImgIndex] = Data[*Index] +
-      ImageData[*ImgIndex - BlockState->BytesPerPixel];
-    *ImgIndex = *ImgIndex + 1;
-  }
-  else
-  {
-    ImageData[*ImgIndex] = Data[*Index];
-    *ImgIndex = *ImgIndex + 1;
-  }
+  image_data[*image_index++] = data[*index];
+  *image_index = *image_index + 1;
 }
 
 static void 
-UpFilter(uint32* Index, uint8* Data, uint32* ImgIndex,
-	 uint8* ImageData, DeflateBlock* BlockState) 
+sub_filter(uint32* index, uint8* data, uint32* image_index, uint8* image_data,
+	  DeflateBlock* block_state) 
 {
-  if (*Index > (BlockState->EndIndexPos -
-		BlockState->Length + BlockState->ScanLineWidth))
+  if (block_state->ScanLinePos > 2)
   {
-    ImageData[*ImgIndex] = Data[*Index] +
-      ImageData[*ImgIndex - BlockState->ScanLineWidth];
-    *ImgIndex = *ImgIndex + 1;
+    image_data[*image_index] = data[*index] +
+      image_data[*image_index - block_state->bytes_per_pixel];
+    *image_index = *image_index + 1;
+    
   }
   else
   {
-    ImageData[*ImgIndex] = Data[*Index];
-    *ImgIndex = *ImgIndex + 1;
+    image_data[*image_index] = data[*index];
+    *image_index = *image_index + 1;
+  }
+}
+
+static void 
+up_filter(uint32* index, uint8* data, uint32* image_index, uint8* image_data,
+	 DeflateBlock* block_state) 
+{
+  if (*index > (block_state->end_index_pos - block_state->length +
+		block_state->scan_line_width))
+  {
+    image_data[*image_index] = data[*index] + image_data[*image_index -
+			      block_state->scan_line_width];
+    *image_index = *image_index + 1;
+    
+  }
+  else
+  {
+    image_data[*image_index] = data[*index];
+    *image_index = *image_index + 1;
   }
   
 }
 
 static void 
-AverageFilter(uint32* Index, uint8* Data, uint32* ImgIndex,
-	      uint8* ImageData, DeflateBlock* BlockState)
+average_filter(uint32* index, uint8* data, uint32* image_index, uint8* image_data,
+	       DeflateBlock* block_state)
 {
-  if (*Index > (BlockState->EndIndexPos - 
-		BlockState->Length + BlockState->ScanLineWidth))
+  
+  if (*index > (block_state->end_index_pos - block_state->length +
+		block_state->scan_line_width))
   {
-    if (BlockState->ScanLinePos > 2)
+    if (block_state->scan_line_pos > 2)
     {
-      ImageData[*ImgIndex] = Data[*Index] +
-	(int)Math_Floor((ImageData[*ImgIndex -
-				   BlockState->BytesPerPixel] +
-			 ImageData[*ImgIndex - BlockState->ScanLineWidth]) * 0.5f);
-      *ImgIndex = *ImgIndex + 1;
+      image_data[*image_index] = data[*index] +
+	(int)Math_Floor((image_data[*image_index - block_state->bytes_per_pixel] +
+			 image_data[*image_index - block_state->scan_line_width]) * 0.5f);
+      *image_index = *image_index + 1;
+      
     }
     else
     {
-      ImageData[*ImgIndex] = Data[*Index] +
-	(int)Math_Floor(ImageData[*ImgIndex -
-				  BlockState->ScanLineWidth] * 0.5f);
-      *ImgIndex = *ImgIndex + 1;
+      image_data[*image_index] = data[*index] +
+	(int)Math_Floor(image_data[*image_index - block_state->scan_line_width] * 0.5f);
+      *image_index = *image_index + 1;
     }
   }
   else
   {
-    if (BlockState->ScanLinePos > 2)
+    if (block_state->scan_line_pos > 2)
     {
-      ImageData[*ImgIndex] = Data[*Index] +
-	(int)Math_Floor((ImageData[*ImgIndex -
-				   BlockState->BytesPerPixel] + 0) * 0.5f);
-      *ImgIndex = *ImgIndex + 1;
+      image_data[*image_index] = data[*index] + 0.5f *
+	(int)Math_Floor((image_data[*image_index - block_state->bytes_per_pixel] + 0));
+      *image_index = *image_index + 1;
+      
     }
     else
     {
-      ImageData[*ImgIndex] = Data[*Index];
-      *ImgIndex = *ImgIndex + 1;
+      image_data[*image_index] = data[*index];
+      *image_index = *image_index + 1;
     }
   }
 }
 
 static void
-PaethFilter(uint32* Index, uint8* Data, uint32* ImgIndex,
-	    uint8* ImageData, DeflateBlock* BlockState)
+paeth_filter(uint32* index, uint8* data, uint32* image_index, uint8* image_data,
+	     DeflateBlock* block_state)
 {
-  if (*Index > (BlockState->EndIndexPos -
-		BlockState->Length + BlockState->ScanLineWidth))
+  if (*index > (block_state->end_index_pos - block_state->length +
+		block_state->scan_line_width))
   {
-    if (BlockState->ScanLinePos > 2)
+    if (block_state->scan_line_pos > 2)
     {
-      ImageData[*ImgIndex] = Data[*Index] +
-	PaethPredictor(ImageData[*ImgIndex -
-				 BlockState->BytesPerPixel],
-		       ImageData[*ImgIndex - BlockState->ScanLineWidth],
-		       ImageData[*ImgIndex - BlockState->ScanLineWidth -
-				 BlockState->BytesPerPixel]);
-      *ImgIndex = *ImgIndex + 1;
+      image_data[*image_index] = data[*index] +
+	paeth_predictor(image_data[*image_index - block_state->bytes_per_pixel],
+		        image_data[*image_index - block_state->scan_line_width],
+		        image_data[*image_index - block_state->scan_line_width -
+				    block_state->bytes_per_pixel]);
+      *image_index = *image_index + 1;
+      
     }
     else
     {
-      ImageData[*ImgIndex] = Data[*Index] + PaethPredictor(0,
-							   ImageData[*ImgIndex - BlockState->ScanLineWidth], 0);
-      *ImgIndex = *ImgIndex + 1;
+      image_data[*image_index] = data[*index] +
+	paeth_predictor(0, image_data[*image_index - block_state->scan_line_width], 0);
+      *image_index = *image_index + 1;
     }
   }
   else
   {
-    if (BlockState->ScanLinePos > 2)
+    
+    if (block_state->scan_line_pos > 2)
     {
-      ImageData[*ImgIndex] = Data[*Index] +
-	PaethPredictor(ImageData[*ImgIndex -
-				 BlockState->BytesPerPixel], 0, 0);
-      *ImgIndex = *ImgIndex + 1;
+      image_data[*image_index] = data[*index] +
+	paeth_predictor(image_data[*image_index - block_state->bytes_per_pixel], 0, 0);
+      *image_index = *image_index + 1;
     }
     else
     {
-      ImageData[*ImgIndex] = Data[*Index] +
-	PaethPredictor(0, 0, 0);
-      *ImgIndex = *ImgIndex + 1;
+      image_data[*image_index] = data[*index] + paeth_predictor(0, 0, 0);
+      *image_index = *image_index + 1;
     }
   }
 }
 
 void
-PNG_Extract(byte* Data, byte** TextureData, uint32* Width, uint32* Height, uint8* BytesPerPixel)
+png_extract(byte* data, byte** texture_data, uint32* width, uint32* height,
+	    uint8* bytes_per_pixel)
 {
-  // TODO: Clean up Data if error.
-  
-  PNGProperties Properties;
-  
-  //NOTE: Confirm that the data is from a PNG file.
-  unsigned long long* Signature = (unsigned long long*)Data;
-  if (*Signature != SIGNATURE)
+  if (!data || !texture_Data || !width || !height || !bytes_per_pixel)
   {
-    //TODO: Debug
+    // ERROR: Invalid inputs
+    // TODO: Change function parameters to avoid this check.
   }
+  PNGProperties properties;
   
-  //NOTE: Read the IHDR chunk.
-  /*
-    Width:              4 bytes
-    Height:             4 bytes
+  unsigned long long* signature = (unsigned long long*)data; 
+  if (*signature != SIGNATURE)
+  {
+    // ERROR: not a png file or an incomplete png header.
+  }
+
+  int32 chunk_length = data[11] | (data[10] << 8) | (data[9] << 16) | (data[8] << 24);
+  int32 chunk_name = data[15] | (data[14] << 8) | (data[13] << 16) | (data[12] << 24);
+  
+  if (chunk_name == IHDR)
+  {
+    
+  /* IHDR header chunk
+    width:              4 bytes
+    height:             4 bytes
     Bit depth:          1 byte
     Color type:         1 byte
     Compression method: 1 byte
     Filter method:      1 byte
     Interlace method:   1 byte
-  */
-  int32 ChunkLength = Data[11] | (Data[10] << 8) | (Data[9] << 16) |
-    (Data[8] << 24);
-  int32 ChunkName = Data[15] | (Data[14] << 8) | (Data[13] << 16) |
-    (Data[12] << 24);
-  
-  if (ChunkName == IHDR)
-  {
-    *Width = (Data[19]) | (Data[18] << 8) | (Data[17] << 16) |
-      (Data[16] << 24);
-    *Height = (Data[23]) | (Data[22] << 8) | (Data[21] << 16) |
-      (Data[20] << 24);
-    Properties.BitDepth = Data[24];
-    Properties.ColorType = Data[25];
-    Properties.CompressionMethod = Data[26];
-    Properties.FilterMethod = Data[27];
-    Properties.InterlaceMethod = Data[28];
-    *BytesPerPixel = Properties.BitDepth / 8;
-    Properties.WidthInBytes = *Width *
-      *BytesPerPixel;
+  */    
+    *width = (data[19]) | (data[18] << 8) | (data[17] << 16) | (data[16] << 24);
+    *height = (data[23]) | (data[22] << 8) | (data[21] << 16) | (data[20] << 24);
+    
+    properties.bit_depth = data[24];
+    properties.color_type = data[25];
+    properties.compression_method = data[26];
+    properties.filter_method = data[27];
+    properties.interlace_method = data[28];
+    *bytes_per_pixel = properties.bit_depth / 8;
+    properties.width_in_bytes = *width * *bytes_per_pixel;
   }
   else
   {
-    //TODO: Debug
+    // ERROR: IHDR must be the first chunk in a valid PNG file.
   }
-  int32 ChunkCRC = Data[32] | (Data[31] << 8) | (Data[30] << 16) |
-    (Data[29] << 24);
   
-  if (*Width && *Height)
+  int32 chunk_crc = data[32] | (data[31] << 8) | (data[30] << 16) | (data[29] << 24);
+  // TODO: Check CRC values
+  
+  if (*width && *height)
   {
+    image_data = 0;
+    image_data = Memory_Allocate(image_data, properties.width_in_bytes *
+				properties.height);
+    *texture_data = new unsigned char[properties.width_in_bytes * *height];
 
-    ImageData = 0;
-    ImageData = Memory_Allocate(ImageData, Properties.WidthInBytes *
-				Properties.Height);
-    *TextureData = new unsigned char[Properties.WidthInBytes *
-				     *Height];
   }
   else
   {
-    //TODO: Debug, Technically not an error.
+    // RETURN: This image is of size 0 by 0
   }
   
-  uint32 i = 33;
-  uint32 ImgIndex = 0;
+  uint32 data_index = 33;
+  uint32 extracted_data_index = 0;
   
   //NOTE: Loop through all chunks
-  bool EndExtraction = false;
-  while (!EndExtraction)
+  bool end_extraction = false;
+  while (!end_extraction)
   {
-    ChunkLength = (Data[i + 3]) | (Data[i + 2] << 8) |
-      (Data[i + 1] << 16) | (Data[i] << 24);
-    ChunkName = Data[i + 7] | (Data[i + 6] << 8) |
-      (Data[i + 5] << 16) | (Data[i + 4] << 24);
+    chunk_length = (data[data_index + 3]) | (data[data_index + 2] << 8) |
+      (data[data_index + 1] << 16) | (data[data_index] << 24);
+    chunk_name = data[data_index + 7] | (data[data_index + 6] << 8) |
+      (data[data_index + 5] << 16) | (data[data_index + 4] << 24);
     
-    i = i + 8;
+    data_index += 8;
     
-    if (ChunkName == IEND)
+    if (chunk_name == IEND)
     {
-      EndExtraction = true;
+      end_extraction = true;
     }
     else
     {
-      if (ChunkName == IDAT)
+      if (chunk_name == IDAT)
       {
 	//NOTE: ZLib Deflate/Inflate Compression information.
 	//TODO: Handle more options from just 78 01 (like 78 9C or 78 DA).
-	uint8 CompressionMethod = Data[i];
-	i++;
+	uint8 compression_method = data[data_index];
+	data_index++;
 	
-	uint8 AdditionalFlags = Data[i];
-	i++;
+	uint8 additional_flags = data[data_index];
+	data_index++;
 	
 	//TODO: Handle the possibility of a DICTID?
 	
-	//NOTE: Index + ChunkLength - Two Flag Bytes - 4 ADLER32 Bytes
-	uint32 ZlibDataLength = i + ChunkLength - 6;
+	//NOTE: index + chunk_lenght  - Two Flag Bytes - 4 ADLER32 Bytes
+	uint32 zlib_data_length = data_index + chunk_length - 6;
 	
-	DeflateBlock BlockState;
-	BlockState.BytesPerPixel = *BytesPerPixel;
-	BlockState.ScanLinePos = Properties.WidthInBytes;// Forcing a line reset
-	BlockState.ScanLineWidth = Properties.WidthInBytes;
+	DeflateBlock block_state;
+	block_state.bytes_per_pixel = *bytes_per_pixel;
+	block_state.scan_line_pos = properties.width_in_bytes;// Forcing a line reset
+	block_state.scan_line_width = properties.width_in_bytes;
 	
 	//NOTE: Loop through the image data (Deflate compress algorithm)
-	while (i < ZlibDataLength)
+	while (data_index < zlib_data_length)
 	{
 	  //NOTE: Block Header
 	  /*
@@ -279,97 +286,98 @@ PNG_Extract(byte* Data, byte** TextureData, uint32* Width, uint32* Height, uint8
 	    10 - Compressed block with the Huffman table supplied.
 	    11 - Reserved.
 	  */
-	  uint8 BlockHeader = Data[i];
-	  i++;
+	  uint8 block_header = data[data_index];
+	  data_index++;
 	  
-	  if ((BlockHeader << 7) == 0x00)
+	  if ((block_header << 7) == 0x00)
 	  {
-	    uint16 BlockLength = (Data[i]) |
-	      (Data[i + 1] << 8);
-	    i = i + 2;
+	    uint16 block_length = (data[data_index]) | (data[data_index + 1] << 8);
+	    data_index += 2;
 	    
-	    uint16 BlockLength1sComplement = (Data[i]) |
-	      (Data[i + 1] << 8);
-	    i = i + 2;
+	    uint16 block_length_ones_complement = (data[data_index]) |
+	      (data[data_index + 1] << 8);
+	    data_index += 2;
 	    
-	    BlockState.Length = BlockLength;
-	    BlockState.EndIndexPos = BlockLength + i;
+	    block_state.length = block_length;
+	    block_state.end_index_pos = block_length + data_index;
 	    
-	    while (i < BlockState.EndIndexPos)
+	    while (data_index < block_state.end_index_pos)
 	    {
-	      if (BlockState.ScanLinePos < BlockState.ScanLineWidth)
+	      if (block_state.scan_line_pos < block_state.scan_line_width)
 	      {
-		switch (BlockState.CurrentFilter)
+		switch (block_state.current_filter)
 		{
 		case 0:
 		  {
-		    NoFilter(&i, Data, &ImgIndex, *TextureData,
-			     &BlockState);
+		    no_filter(&data_index, data, &extracted_data_index, *texture_data,
+			      &block_state);
+		    
 		  } break;
 		case 1:
 		  {
-		    SubFilter(&i, Data, &ImgIndex,
-			      *TextureData, &BlockState);
+		    sub_filter(&data_index, data, &extracted_data_index, *texture_data,
+			       &block_state);
 		  } break;
 		case 2:
 		  {
-		    UpFilter(&i, Data, &ImgIndex,
-			     *TextureData, &BlockState);
+		    up_filter(&data_index, data, &extracted_data_index, *texture_data,
+			      &block_state);
 		  } break;
 		case 3:
 		  {
-		    AverageFilter(&i, Data, &ImgIndex,
-				  *TextureData, &BlockState);
+		    average_filter(&data_index, data, &extracted_data_index, *texture_data,
+				   &block_state);
 		  } break;
 		case 4:
 		  {
-		    PaethFilter(&i, Data, &ImgIndex,
-				*TextureData, &BlockState);
+		    paeth_filter(&data_index, data, &extracted_data_index, *texture_data,
+				 &block_state);
+
 		  } break;
 		default:
 		  {
 		    //TODO: Error Handle
 		  } break;
 		}
-		BlockState.ScanLinePos++;
-		i++;
+		block_state.scan_line_pos++;
+		data_index++;
 	      }
 	      else // NOTE: New scanline
 	      {
-		BlockState.CurrentFilter = Data[i];
-		BlockState.ScanLinePos = 0;
-		i++;
+		block_state.current_filter = data[data_index];
+		block_state.scan_line_pos = 0;
+		data_index++;
 	      }
 	    }
 	  }
-	  else if ((BlockHeader << 7) == 0x80)
+	  else if ((block_header << 7) == 0x80)
 	  {
-	    int16 BlockLength = (Data[i]) |
-	      (Data[i + 1] << 8);
-	    i = i + 2;
+	    int16 block_length = (data[data_index]) | (data[data_index + 1] << 8);
+	    data_index += 2;
 	    
-	    int16 BlockLength1sComplement = (Data[i]) |
-	      (Data[i + 1] << 8);
-	    i = i + 2;
+	    int16 block_length_ones_complement = (data[data_index]) |
+	      (data[data_index + 1] << 8);
+	    data_index += 2;
 	  }
 	}
 	
-	int32 ADLER32Checksum = (Data[i + 3]) | (Data[i + 2] << 8) |
-	  (Data[i + 1] << 16) | (Data[i] << 24);
-	i = i + 4;
+	int32 adler_32_checksum = (data[data_index + 3]) | (data[data_index + 2] << 8) |
+	  (data[data_index + 1] << 16) | (data[data_index] << 24);
+	data_index += 4;
       }
-      else if (i == 900000)
+      else if (data_index == 900000)
       {
 	break; //TODO: Debug
       }
       else
       {
 	//NOTE: Increment the index past the chunk data.
-	i = i + ChunkLength;
+	data_index += chunk_length;
       }
-      ChunkCRC = Data[i + 3] | (Data[i + 2] << 8) |
-	(Data[i + 1] << 16) | (Data[i] << 24);;
-      i = i + 4;
+      
+      chunk_crc = data[data_index + 3] | (data[data_index + 2] << 8) |
+	(data[data_index + 1] << 16) | (data[data_index] << 24);
+      data_index += 4;
     }
   }
 }
